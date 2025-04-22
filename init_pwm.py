abb3a9a6e07e160a2974077b87a018b5a710a568
aidcol <aidancolon20@gmail.com>
2025-04-21 21:38:18 -0500

    Refactor GPIO setup

--- init_pwm.py
+++ init_pwm.py
@@ -3,30 +3,29 @@ import time
 import RPi.GPIO as GPIO
 
 
-def setup_gpio():
-    GPIO.setmode(GPIO.BCM)
+GPIO.setmode(GPIO.BCM)
 
-    # Speed control
-    speedPin = 18
-    speed_pwm_hz = 50
-    speed_duty_cycle = 50
-    GPIO.setup(speedPin, GPIO.OUT)
-    speed = GPIO.PWM(speedPin, speed_pwm_hz)
-    speed.start(speed_duty_cycle)
+# Speed control
+speedPin = 18
+speed_pwm_hz = 50
+speed_dc = 7.5
+GPIO.setup(speedPin, GPIO.OUT)
+speed = GPIO.PWM(speedPin, speed_pwm_hz)
+speed.start(speed_dc)
 
-    # Steering control
-    steeringPin = 19
-    steering_pwm_hz = 50
-    steering_duty_cycle = 7.5
-    GPIO.setup(steeringPin, GPIO.OUT)
-    steering = GPIO.PWM(steeringPin, steering_pwm_hz)
-    steering.start(steering_duty_cycle)
+# Steering control
+steeringPin = 19
+steering_pwm_hz = 50
+steering_dc = 7.5
+GPIO.setup(steeringPin, GPIO.OUT)
+steering = GPIO.PWM(steeringPin, steering_pwm_hz)
+steering.start(steering_dc)
 
-
-if __name__ == '__main__':
-    print('Initializing PWM...')
-    setup_gpio()
-
+try:
     while True:
-        time.sleep(1)
-        print('hey')
+        time.sleep(10)
+except KeyboardInterrupt:
+    speed.stop()
+    steering.stop()
+    GPIO.cleanup()
+
